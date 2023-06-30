ae00eaf9f185c5a7a5f1c165d64f9a4592b42efb
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-06-30 22:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateTextWithPromise() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+updateTextWithPromise().then((message) => {
+  const outputElement = document.getElementById("output");
+  outputElement.textContent = message;
+});
